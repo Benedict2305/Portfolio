e205e73fc48159874703840d84cf1353f5c3a610
BENEDICT <benidzy.bi@gmail.com>
2025-12-01 23:45:52 +0100

    updated navbar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React from 'react'
-// import Navbar from './navigation/Navbar'
+import Navbar from './navbar/Navbar'
 import Hero from './hero/Hero'
 import About from './about/About'
 import MyWork from './myWork/MyWork'
 import Contact from './contact/Contact'
+import Footer from './navbar/Footer'
 
 const App = () => {
   return (
     <div>
-    {/* <Navbar /> */}
+    <Navbar />
     <Hero />
     <About />
     <MyWork />
     <Contact />
-    {/* <Footer/> */}
+    <Footer/>
     </div>
   )
 }
